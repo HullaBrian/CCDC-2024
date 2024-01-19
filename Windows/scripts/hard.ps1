0abcbb7578f09d4ab920c34cfbed37a2071ebebe
hullabrian <beierlejc@gmail.com>
2024-01-19 17:51:34 -0600

    Finished windows enumeration script

---
+++ Windows/scripts/hard.ps1
@@ -0,0 +1,17 @@
+﻿Function InstallHardeningKitty() {
+    $Version = (((Invoke-WebRequest "https://api.github.com/repos/scipag/HardeningKitty/releases/latest" -UseBasicParsing) | ConvertFrom-Json).Name).SubString(2)
+    $HardeningKittyLatestVersionDownloadLink = ((Invoke-WebRequest "https://api.github.com/repos/scipag/HardeningKitty/releases/latest" -UseBasicParsing) | ConvertFrom-Json).zipball_url
+    $ProgressPreference = 'SilentlyContinue'
+    Invoke-WebRequest $HardeningKittyLatestVersionDownloadLink -Out HardeningKitty$Version.zip
+    Expand-Archive -Path ".\HardeningKitty$Version.zip" -Destination ".\HardeningKitty$Version" -Force
+    $Folder = Get-ChildItem .\HardeningKitty$Version | Select-Object Name -ExpandProperty Name
+    Move-Item ".\HardeningKitty$Version\$Folder\*" ".\HardeningKitty$Version\"
+    Remove-Item ".\HardeningKitty$Version\$Folder\"
+    New-Item -Path $Env:ProgramFiles\WindowsPowerShell\Modules\HardeningKitty\$Version -ItemType Directory -Force
+    Set-Location .\HardeningKitty$Version
+    Copy-Item -Path .\HardeningKitty.psd1,.\HardeningKitty.psm1,.\lists\ -Destination $Env:ProgramFiles\WindowsPowerShell\Modules\HardeningKitty\$Version\ -Recurse -Force
+    Import-Module "$Env:ProgramFiles\WindowsPowerShell\Modules\HardeningKitty\$Version\HardeningKitty.psm1"
+}
+InstallHardeningKitty
+
+Write-Host "`n`nSUCCESSFULLY INSTALLED HARDENINGKITTY!`n`n"
--- Windows/scripts/inventory.ps1
+++ Windows/scripts/inventory.ps1
@@ -1,130 +1,172 @@
-# HOSTNAME
-    # cmd: hostname
-    # powershell command: [System.Net.Dns]::GetHostName()
-    # environment variables: $env:COMPUTERNAME
-$name = hostname
-Write-Host "Workstation / VM Identification: '$name'"
-
-
-
-# OS version
-$os_version = (Get-WmiObject -class Win32_OperatingSystem).Caption
-Write-Host "OS Version: '$os_version'"
-
-
-
-# Services
-#$services = Get-Process | Where-Object { $_.Company -notlike 'Microsoft*' }
-#$services | Select-Object Id, ProcessName, Company
-Write-Host ""
-Write-Host "Services:"
-Write-Host "---------"
-$services = Get-Process | Where-Object {
-    $_.Company -notlike 'Microsoft*' -and
-    $_.ProcessName -ne 'svchost' -and
-    $_.ProcessName -ne 'conhost' -and
-    $_.ProcessName -ne 'csrss' -and
-    $_.ProcessName -ne 'Registry'
+﻿# Gather info
+$hostinfo = Get-ComputerInfo
+$netinfo = Get-NetIPConfiguration -Detailed
+$domain = (Get-WmiObject Win32_ComputerSystem).Domain
+# Get Enabled Windows Features
+if ( $hostinfo.WindowsProductName.Contains("Server")){
+    # Machine is running Windows Server
+    $featurelist = Get-WindowsFeature | Where-Object {$_.Installed -eq "Installed"} 
+} else {
+    # Machine is running Windows 10
+    $featurelist = Get-WindowsCapability -Online | Where-Object {$_.State -eq "Enabled"} | Select-Object FeatureName
 }
-$tmp = $services | Select-Object Id, ProcessName | Out-String
-Write-Host $tmp
+                                             
+Write-output "================"
+Write-output "██████████  ██████      ████    ██      ██  " 
+Write-output "   ██      ██   ███   ██   ██  ████    ██   " 
+Write-output "  ██      ██████     ██   ██  ██  ██  ██    " 
+Write-output " ██      ██    ██   ██   ██  ██    ████     " 
+Write-output "██      ██    ██    █████   ██      ██      "
+Write-output "Target Recognition Operator Notification (TRON) system"
+
+# Hostname
+Write-output "================"
+Write-output "HOSTNAME: $($hostinfo.CsDomain)\$($hostinfo.CsName)"
 
 
+# Operating System
+Write-output "================"
+Write-output "OS: $($hostinfo.WindowsProductName) - $($hostinfo.OSVersion) - $($hostinfo.OsBuildNumber)"
+foreach( $hotfix in $hostinfo.OsHotFixes){
+   Write-Output "$($hotfix.HotFixID) - $($hotfix.Description) - $($hotfix.InstalledOn)"
+}
 
-# Open Ports
-Write-Host ""
-Write-Host "Open ports:"
-Write-Host "-----------"
-# Get all open TCP and UDP connections
-$openTcpPorts = Get-NetTCPConnection -State Listen
-$openUdpPorts = Get-NetUDPEndpoint
-$output = @()
 
-# Display open TCP ports with corresponding applications
-foreach ($tcpPort in $openTcpPorts) {
-    $processId = $tcpPort.OwningProcess
-    $process = Get-Process -Id $processId -ErrorAction SilentlyContinue
+# Network Interfaces
+Write-output "================"
+Write-output "Network Interfaces:"
+foreach( $interface in $netinfo ) {
+    Write-Output "- $($interface.InterfaceAlias)"
+    Write-Output "    - MAC: $($interface.NetAdapter.LinkLayerAddress)"
+    Write-Output "    - IPv4: $($interface.IPv4Address.Ipv4Address)"
+    Write-Output "    - IPv6: $($interface.IPv6Address.IPv6Address)"
+    Write-Output "    - Default gateway: $($interface.IPv4DefaultGateway.NextHop)"
+    Write-Output "    - DNS: $($interface.DNSServer.ServerAddresses)"
+}
 
-    $output += [PSCustomObject]@{
-        'Protocol'     = 'TCP'
-        'LocalAddress' = $tcpPort.LocalAddress
-        'LocalPort'    = $tcpPort.LocalPort
-        'Application'  = if ($process) { $process.ProcessName } else { 'N/A' }
+
+# Purpose
+Write-output "================"
+Write-output "Purpose:"
+Write-output "Assess what this system's purpose is with context to business operations."
+Write-output "Is it a web server? What context does it host? Who would access it (public or intranet)?"
+Write-output "Part of domain? $($hostinfo.CsPartOfDomain) - $($hostinfo.CsDomainRole)"
+$isDomainJoined = $domain -ne $null -and $domain -ne ""
+Write-Output "Domain joined? - $($isDomainJoined)"
+
+# Pause space
+# Write-output "================"
+# Write-output "Press enter to continue"
+# read-host
+
+# Active Services
+Write-output "================"
+Write-output "Installed Features:"
+Write-output "~~~~~~~~~~~~~~~~~~~"
+foreach( $feature in $featurelist){
+   Write-Output $feature.DisplayName
+}
+
+Write-output "----------------"
+
+# Installed Applications:
+Write-output "Installed Applications:"
+Write-output "~~~~~~~~~~~~~~~~~~~~~~~"
+$applist = Get-WmiObject -Class Win32_Product | select Name,Vendor,Version
+foreach( $app in $applist){
+   Write-Output "$($app.Name) | ($($app.Vendor))"
+   Write-Output "- $($app.Version)"
+}
+
+# Pause space
+# Write-output "================"
+# Write-output "Press enter to continue"
+# read-host
+
+
+# Ports
+Write-output "================"
+Write-output "Processes:"
+$processList = Get-CimInstance Win32_Process
+foreach ($process in $processList) {
+    $portList = Get-NetTCPConnection -OwningProcess $process.ProcessId -ErrorAction SilentlyContinue |
+        Select-Object LocalAddress,LocalPort,RemoteAddress,RemotePort
+    if ($portList) {
+        if ($process.ProcessName -eq 'svchost.exe') {
+            $service = Get-WmiObject -Class Win32_Service -Filter "ProcessID=$($process.ProcessId)" | Select-Object -Property Name 
+            Write-Output "$($process.ProcessName) ($($process.ProcessId)) - $($service.Name)"
+            Write-Output "- $($process.CommandLine)"
+            foreach( $port in $portList){
+                if($port.LocalAddress -ne "::1" -and $port.LocalAddress -ne "127.0.0.1"){ 
+                    Write-Output "  - $($port.LocalAddress):$($port.LocalPort) - $($port.RemoteAddress):$($port.RemotePort)"
+                }
+            }
+        }
+        else {
+            $service = Get-WmiObject -Class Win32_Service -Filter "ProcessID=$($process.ProcessId)" 
+            Write-Output "$($process.ProcessName) ($($process.ProcessId))"
+            Write-Output "- $($process.CommandLine)"
+            foreach( $port in $portList){
+                if($port.LocalAddress -ne "::1" -and $port.LocalAddress -ne "127.0.0.1"){ 
+                    Write-Output "  - $($port.LocalAddress):$($port.LocalPort) - $($port.RemoteAddress):$($port.RemotePort)"
+                }
+            }
+        }
+        write-output ""
     }
 }
 
-# Display open UDP ports with corresponding applications
-foreach ($udpPort in $openUdpPorts) {
-    $processId = $udpPort.OwningProcess
-    $process = Get-Process -Id $processId -ErrorAction SilentlyContinue
+# Pause space
+# Write-output "================"
+# Write-output "Press enter to continue"
+# read-host
 
-    $output += [PSCustomObject]@{
-        'Protocol'     = 'UDP'
-        'LocalAddress' = $udpPort.LocalAddress
-        'LocalPort'    = $udpPort.LocalPort
-        'Application'  = if ($process) { $process.ProcessName } else { 'N/A' }
+
+#Users
+Write-output "================"
+Write-output "Local Users:"
+Get-LocalUser | Select-Object Name, PrincipalSource
+
+$privgroups = (Get-ACL "AD:\$($(get-addomain).DistinguishedName)").Access | where-object {($_.ActiveDirectoryRights -ne "ReadProperty") -and ($_.ActiveDirectoryRights -ne "ListChildren") -and ($_.ActiveDirectoryRights -ne "DeleteChild") -and ($_.ActiveDirectoryRights -ne "GenericRead") -and ($_.ActiveDirectoryRights -ne "readProperty, ReadControl")} | Select-Object -Property IdentityReference -Unique 
+foreach( $group in $privgroups){
+    $t2 = $group.IdentityReference.ToString().split("\")
+    try{
+        Get-ADGroupMember -Identity $t2[1] -Recursive  | Format-Table name,distinguishedName,objectClass
     }
-}
-Write-Host ($output | Format-Table -AutoSize | Out-String)
-
-
-
-# IP Addresses
-Write-Host "Network Configuration:"
-Write-Host "----------------------`n"
-# Get network interfaces
-$networkInterfaces = Get-NetIPConfiguration
-
-# Display information in a table
-foreach ($interface in $networkInterfaces) {
-    Write-Host "Network Interface: $($interface.InterfaceAlias)"
-    
-    $ipv4Info = $interface | Select-Object InterfaceIndex, InterfaceAlias, 
-                @{Name='IPv4Address'; Expression={$_.IPv4Address.IPAddress}},
-                @{Name='SubnetMask'; Expression={
-                    if ($_.IPv4SubnetMask) {
-                        ($_.IPv4SubnetMask.IPAddress -as [IPAddress]).IPAddressToString
-                    } else {
-                        'N/A'
-                    }
-                }},
-                @{Name='DefaultGateway'; Expression={$_.IPv4DefaultGateway.NextHop}},
-                @{Name='DNSServers'; Expression={$_.DNSServer.ServerAddresses -join ', '}}
-
-    $ipv4Info | Format-Table -AutoSize
-    Write-Host "`n"  # Add a newline for separation
+    catch{}
 }
 
+if ($isDomainJoined) {
+    Write-output "================"
+    Write-output "`nDomain Users`n-----------------"
+    $users = Get-ADUser -Filter *
+    $userInfoArray = @()
 
+    foreach ($user in $users) {
+        $userDN = $user.DistinguishedName
+        $ou = $userDN -replace '^CN=[^,]+,'
+        $groups = Get-ADUser $user | Get-ADPrincipalGroupMembership | Select-Object -ExpandProperty Name
 
-# Get local user accounts
-Write-Host "`nUsers`n--------------"
-$localUsers = Get-WmiObject Win32_UserAccount | Where-Object { $_.LocalAccount -eq $true }
-
-# Display user information and groups in a table
-$usersWithGroups = $localUsers | ForEach-Object {
-    $user = $_
-    $groups = ([adsi]"WinNT://./$($user.Name),user").Groups() | ForEach-Object {
-        $_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null)
+        $userInfo = [PSCustomObject]@{
+            'Username' = $user.SamAccountName
+            'OU' = $ou
+            'Groups' = $groups -join ', '
+        }
+        $userInfoArray += $userInfo
     }
 
-    [PSCustomObject]@{
-        'Caption'   = $user.Caption
-        'Username'  = $user.Name
-        'Groups'    = $groups -join ', '
-    }
+    $userInfoArray | Format-Table -AutoSize
 }
 
-$usersWithGroups | Format-Table -AutoSize
 
-
-
-# Get startup tasks
-Write-Host "`nStartup Tasks`n--------------------"
+# Startup Tasks
+Write-Output "=================="
+Write-Output "Startup Tasks:"
 Get-CimInstance Win32_StartupCommand | Select-Object Name, Command, Location, User | Format-Table -AutoSize
+Write-Output "=================="
 
 
-
-# Get shares
-Write-Host "`nShares`n--------"
+# Shares
+Write-Output "SMB Shares:"
 Get-SmbShare | Format-Table -Property Name, Path, Description, FullAccess, ReadAccess -AutoSize
+Write-Output "==================="
